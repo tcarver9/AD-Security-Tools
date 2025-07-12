0eead50042fb97ab9b86d762159f1cb5de56492a
T73 <142630595+tcarver9@users.noreply.github.com>
2025-07-12 09:19:48 -0400

    Update enum-users.ps1

--- enum-users.ps1
+++ enum-users.ps1
@@ -1,1 +1,3 @@
-
+Get-ADUser -Filter * -Properties DisplayName, Enabled, LastLogonDate, PasswordNeverExpires |
+Select-Object Name, Enabled, LastLogonDate, PasswordNeverExpires |
+Export-Csv -Path "./sample-output/users.csv" -NoTypeInformation
