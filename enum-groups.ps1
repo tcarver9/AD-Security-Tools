4880d28b77256a03dcbab9ec79634b7f7aeb2f68
T73 <142630595+tcarver9@users.noreply.github.com>
2025-07-12 09:20:32 -0400

    Update enum-groups.ps1

--- enum-groups.ps1
+++ enum-groups.ps1
@@ -1,1 +1,7 @@
+$groups = @("Domain Admins", "Enterprise Admins", "Administrators")
 
+foreach ($group in $groups) {
+    Get-ADGroupMember -Identity $group |
+    Select-Object Name, SamAccountName, objectClass |
+    Export-Csv -Path "./sample-output/${group}-members.csv" -NoTypeInformation
+}
