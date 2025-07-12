61170f75032da66ef50c1b6a69fde3ea3a85a418
T73 <142630595+tcarver9@users.noreply.github.com>
2025-07-12 09:22:19 -0400

    Update delegation-check.ps1

--- delegation-check.ps1
+++ delegation-check.ps1
@@ -1,1 +1,3 @@
-
+Get-ADComputer -Filter 'TrustedForDelegation -eq $true' -Properties TrustedForDelegation |
+Select-Object Name, OperatingSystem, TrustedForDelegation |
+Export-Csv -Path "./sample-output/delegation.csv" -NoTypeInformation
