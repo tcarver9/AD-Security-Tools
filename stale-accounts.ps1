ec3d0ceb3ddbbe9828ba268e42db204a38b06405
T73 <142630595+tcarver9@users.noreply.github.com>
2025-07-12 09:21:36 -0400

    Update stale-accounts.ps1

--- stale-accounts.ps1
+++ stale-accounts.ps1
@@ -1,1 +1,8 @@
+$threshold = (Get-Date).AddDays(-90)
+
+Get-ADUser -Filter * -Properties LastLogonDate |
+Where-Object { $_.LastLogonDate -lt $threshold } |
+Select-Object Name, LastLogonDate |
+Export-Csv -Path "./sample-output/stale-users.csv" -NoTypeInformation
+
 
