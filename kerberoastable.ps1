610cd66e1422cce536d11d8b835fd153707d001a
T73 <142630595+tcarver9@users.noreply.github.com>
2025-07-12 09:21:02 -0400

    Update kerberoastable.ps1

--- kerberoastable.ps1
+++ kerberoastable.ps1
@@ -1,1 +1,3 @@
-
+Get-ADUser -Filter {ServicePrincipalName -like "*"} -Properties ServicePrincipalName |
+Select-Object Name, SamAccountName, ServicePrincipalName |
+Export-Csv -Path "./sample-output/kerberoastable.csv" -NoTypeInformation
