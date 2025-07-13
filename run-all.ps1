506f4b1603bcc0d3d48924e6ec2f210035e05d48
T73 <142630595+tcarver9@users.noreply.github.com>
2025-07-13 17:24:30 -0400

    Create run-all.ps1

---
+++ run-all.ps1
@@ -0,0 +1,5 @@
+.\enum-users.ps1 | Out-File enum-users.txt
+.\enum-groups.ps1 | Out-File enum-groups.txt
+.\kerberoastable.ps1 | Out-File kerberoastable.txt
+.\stale-accounts.ps1 | Out-File stale-accounts.txt
+.\delegation-check.ps1 | Out-File delegation-check.txt
